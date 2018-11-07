97be88c30d3711962966dbc7029926c4d4a123d4
zcreturn0 <237350543@qq.com>
2018-11-07 10:51:46 +0800

    create,drop table

--- mysql.js
+++ mysql.js
@@ -4,17 +4,42 @@ var connection = mysql.createConnection({
     host:'localhost',
     port:3306,
     user:'root',
-    password:'root'
+    password:'root',
+    //数据库操作不加下面这行
+    database:'learning'
 });
 
 connection.connect();
 
-connection.query('create database learning',function(err,results,fields){
+var sql;
+var successInfo;
+// 创建数据库
+// sql = 'create database learning';
+// successInfo = 'database learning created';
+
+// 删除数据库
+// sql = 'drop database test';
+// successInfo = 'database test droped';
+
+// 创建表格
+// sql = `CREATE TABLE IF NOT EXISTS stu(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(10) NOT NULL,
+//     age INT NOT NULL,
+//     addr VARCHAR(255)
+// )`;
+// successInfo = 'table stu created';
+
+// 删除表格
+// sql = 'DROP TABLE aaa';
+// successInfo = 'table aaa droped';
+
+connection.query(sql,function(err,results,fields){
     if(err){
         console.log(err);
     }
     else{
-        console.log('database learning created');
+        console.log(successInfo);
     }
 })
 
